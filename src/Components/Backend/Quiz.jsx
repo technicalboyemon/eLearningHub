aad6173ecbb1ca1035beaf35485919d8bf475aac
shahsajib95 <sajib.shahariar97@gmail.com>
2022-06-19 21:10:52 +0600

    add

--- src/Components/Backend/Quiz.jsx
+++ src/Components/Backend/Quiz.jsx
@@ -1,94 +1,237 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import { getAll } from "../../api/api";
+
+const arr = ["answer1", "answer2", "answer3", "answer4"];
 
 const Quiz = () => {
-  const initialstate = {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const fetch = await getAll("users");
+      setUsers(fetch);
+    };
+    getUser();
+  }, []);
+
+  const [students, setStudents] = useState([]);
+  const [subject, setSubject] = useState("");
+  const [questions, setQuestions] = useState([]);
+
+  const initialState = {
+    id: nanoid(),
     question: "",
-    answer1: { bol: false, title: "" },
-    answer2: { bol: false, title: "" },
-    answer3: { bol: false, title: "" },
-    answer4: { bol: false, title: "" },
+    ans: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
   };
 
-  const [questions, setQuestions] = useState([initialstate]);
+  const [quiz, setQuiz] = useState(initialState);
 
-  const questionHandle = (e, id) => {
-    const oldValue = [...questions];
-    oldValue[id].question = e.target.value;
-    setQuestions(oldValue);
+  const handleQuiz = (e) => {
+    setQuiz({ ...quiz, [e.target.name]: e.target.value });
   };
-  const answer1Handle = (e, id) => {
-    const oldValue = [...questions];
-    oldValue[id].answer1.title = e.target.value;
-    setQuestions(oldValue);
+
+  const adQuiztoui = () => {
+    if (
+      !quiz.question ||
+      !quiz.answer1 ||
+      !quiz.answer2 ||
+      !quiz.answer3 ||
+      !quiz.answer4 ||
+      !quiz.ans
+    )
+      return;
+    setQuestions([...questions, quiz]);
+    setQuiz(initialState);
   };
-  const answer2Handle = (e, id) => {
-    const oldValue = [...questions];
-    oldValue[id].answer2.title = e.target.value;
-    setQuestions(oldValue);
+
+  const getValueInput = (data) => {
+    switch (data) {
+      case "answer1":
+        return quiz.answer1;
+      case "answer2":
+        return quiz.answer2;
+      case "answer3":
+        return quiz.answer3;
+      case "answer4":
+        return quiz.answer4;
+    }
   };
-  const answer3Handle = (e, id) => {
-    const oldValue = [...questions];
-    oldValue[id].answer3.title = e.target.value;
-    setQuestions(oldValue);
+
+  const [edit, setEdit] = useState(null);
+  const handleEdit = (idx) => {
+    setEdit(idx);
+    setQuiz(questions.find((i, ix) => ix == idx));
   };
-  const answer4Handle = (e, id) => {
-    const oldValue = [...questions];
-    oldValue[id].answer4.title = e.target.value;
-    setQuestions(oldValue);
+
+  const updateQuiz = () => {
+    if (
+      !quiz.question ||
+      !quiz.answer1 ||
+      !quiz.answer2 ||
+      !quiz.answer3 ||
+      !quiz.answer4 ||
+      !quiz.ans
+    )
+      return;
+    const quizes = questions.filter((i, ix) => ix != edit);
+    quizes.splice(edit, 0, quiz);
+    console.log(quizes);
+    setQuestions(quizes);
+    setEdit(null);
+    setQuiz(initialState);
   };
-  console.log(questions);
+
+  console.log(students);
+
   return (
     <div>
+      <div className="my-2">
+        <label className="form-label fw-bold">Select Users</label>
+        <select
+          className="form-select"
+          onChange={(e) =>
+            setStudents([
+              ...students,
+              users.find((i) => i._id == e.target.value),
+            ])
+          }
+        >
+          {users?.map((i) => (
+            <>
+              <option selected>Add Students</option>
+              <option value={i?._id}>{i?.name}</option>
+            </>
+          ))}
+        </select>
+      </div>
+      <div className="d-flex">
+        {students.map((i) => (
+          <p className="p-2 bg-secondary m-2 text-white">{i?.name}</p>
+        ))}
+      </div>
+      <div className="my-2">
+        <label className="form-label fw-bold">Subject</label>
+        <input
+          onChange={(e) => setSubject(e.target.value)}
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <h1 className="mt-5">{subject}</h1>
       {questions.map((i, idx) => (
         <div>
-          <h4>{i.question}</h4>
-          <div>
-            <div className="row">
-              <div className="col-md-3">{i.answer1.title}</div>
-              <div className="col-md-3">{i.answer2.title}</div>
-              <div className="col-md-3">{i.answer3.title}</div>
-              <div className="col-md-3">{i.answer4.title}</div>
+          <div className="my-2 bg-white p-2">
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={() => {
+                setQuestions(questions.filter((i, ix) => ix != idx));
+              }}
+            >
+              Delete
+            </button>
+            <button
+              className="btn btn-primary btn-sm ms-2"
+              onClick={() => {
+                handleEdit(`${idx}`);
+              }}
+            >
+              Edit
+            </button>
+            <br></br>
+            <div className="fw-bold text-primary my-2">
+              <span>Question {idx + 1}</span>
+            </div>
+            <h4>{i.question}</h4>
+            <div>
+              <div className="row">
+                <div className="col-md-3">
+                  <input
+                    type="checkbox"
+                    checked={i.ans == "answer1"}
+                    className="mx-2 form-check-input"
+                  />
+                  1. {i.answer1}
+                </div>
+                <div className="col-md-3">
+                  <input
+                    type="checkbox"
+                    checked={i.ans == "answer2"}
+                    className="mx-2 form-check-input"
+                  />
+                  2. {i.answer2}
+                </div>
+                <div className="col-md-3">
+                  <input
+                    type="checkbox"
+                    checked={i.ans == "answer3"}
+                    className="mx-2 form-check-input"
+                  />
+                  3. {i.answer3}
+                </div>
+                <div className="col-md-3">
+                  <input
+                    type="checkbox"
+                    checked={i.ans == "answer4"}
+                    className="mx-2 form-check-input"
+                  />
+                  4. {i.answer4}
+                </div>
+              </div>
             </div>
           </div>
         </div>
       ))}
-      {questions.map((i, idx) => (
-        <div key={idx}>
-          <input
-            onChange={(e) => questionHandle(e, idx)}
-            value={i.question}
-            type="text"
-            name="question"
-          />
-          <input
-            onChange={(e) => answer1Handle(e, idx)}
-            value={i.answer1.title}
-            type="text"
-            name="answer1"
-          />
-          <input
-            onChange={(e) => answer2Handle(e, idx)}
-            value={i.answer2.title}
-            type="text"
-            name="answer2"
-          />
-          <input
-            onChange={(e) => answer3Handle(e, idx)}
-            value={i.answer3.title}
-            type="text"
-            name="answer3"
-          />
-          <input
-            onChange={(e) => answer4Handle(e, idx)}
-            value={i.answer4.title}
-            type="text"
-            name="answer4"
-          />
+
+      <div className="my-5">
+        <label className="form-label fw-bold">Question</label>
+        <input
+          onChange={(e) => handleQuiz(e)}
+          type="text"
+          name="question"
+          className="form-control"
+          value={quiz?.question}
+        />
+        <div className="row justify-content-between align-items-center my-2">
+          {arr.map((i, idx) => (
+            <div className="col-md-3">
+              <label className="form-label">{idx + 1}</label>
+              <div className="d-flex justify-content-between align-items-center">
+                <input
+                  type="checkbox"
+                  checked={quiz.ans == i}
+                  className="mx-2 form-check-input"
+                  onChange={() => setQuiz({ ...quiz, ans: i })}
+                />
+                <input
+                  onChange={(e) => handleQuiz(e)}
+                  type="text"
+                  name={i}
+                  className="form-control"
+                  value={getValueInput(i)}
+                />
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-      <button onClick={() => setQuestions([...questions, initialstate])}>
-        Add
-      </button>
+      </div>
+
+      {edit ? (
+        <button onClick={() => updateQuiz()} className="btn btn-primary">
+          Update Quiz
+        </button>
+      ) : (
+        <button
+          onClick={() => adQuiztoui()}
+          className="btn btn-warning text-white"
+        >
+          Add Quiz
+        </button>
+      )}
     </div>
   );
 };
---
+++ src/api/api.js
@@ -0,0 +1,100 @@
+import axios from "axios";
+export const baseUrl = "http://localhost:5000";
+
+export const getAll = async (url) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "GET",
+  });
+  const data = await res.json();
+  return data;
+};
+
+export const getFetchData = async (url, token) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export const postData = async (url, post, token) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(post),
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export const formDataPost = async (url, post, token) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+    body: post,
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export const putData = async (url, post, token) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(post),
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export const patchData = async (url, post, token) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(post),
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export const deleteData = async (url, token) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export const uploadFile = async (url, file, token) => {
+  const res = await fetch(`${baseUrl}/${url}`, {
+    method: "POST",
+    body: file,
+  });
+
+  const data = await res.json();
+  return data;
+};
